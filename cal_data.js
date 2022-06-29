4c78b0ff54fdcff2bcada9184c590453e177a021
YounmiSon <https://github.com/YounmiSon>
2022-06-29 10:48:02 +0900

    team2

---
+++ cal_data.js
@@ -0,0 +1,30 @@
+let events = [
+    {
+        idx: 1, 
+        content: 'test'
+    },
+    {
+        idx: 2, 
+        content: 'test'
+    },
+    {
+        idx: 3, 
+        content: 'test'
+    },
+    {
+        idx: 4, 
+        content: 'test'
+    },
+    {
+        idx: 5, 
+        content: 'test'
+    },
+    {
+        idx: 6, 
+        content: 'test'
+    },
+    {
+        idx: 7, 
+        content: 'test'
+    },
+]
