2def2862333615a916a87b61d2b36d23041e29f1
YounmiSon <https://github.com/YounmiSon>
2022-07-13 11:35:56 +0900

    후원하기수정

--- js/donate.js
+++ js/donate.js
@@ -3,7 +3,7 @@ let Text = document.querySelector('.text_pack')
 window.addEventListener('scroll', function(){
     let value = window.scrollY
     console.log(value);
-    if(value > 6200){
+    if(value > 6300){
         Text.style.visibility = 'visible'
     }
     else if(value < 7129){
@@ -12,7 +12,9 @@ window.addEventListener('scroll', function(){
 })
 
 //후원하기 누르면 숫자 랜덤으로 증가
-document.querySelector(".dnBtn").onclick = function(){
+const aniBtn = document.querySelector(".w-btn-outline")
+aniBtn.onclick = function(){
     let a = document.querySelector(".donatedMoney")
     a.innerHTML = Number(a.innerHTML) + 1
+    a.style.color = "#ffbb2e"
 }
